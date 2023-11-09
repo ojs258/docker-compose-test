d1a59d8c3c7df395736f15a7c1be25b2da8920ed
oh jinseok <ojs258@naver.com>
2023-11-09 11:14:45 +0900

    init

---
+++ composeTest1/src/main/java/com/example/composetest1/MsgDto.java
@@ -0,0 +1,8 @@
+package com.example.composetest1;
+
+import lombok.Data;
+
+@Data
+public class MsgDto {
+    String msg = "compose push test 1";
+}
---
+++ composeTest1/src/test/java/com/example/composetest1/ComposeTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.composetest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
