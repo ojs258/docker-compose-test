ea61c485c53478fb4d5915a0e23b7fd2f7339c1a
oh jinseok <ojs258@naver.com>
2023-11-09 21:24:08 +0900

    2023-11-09 특정 인스턴스에서 Dockerfile *와일드카드 안먹는거 해결중

--- composeTest2/src/main/java/com/example/composetest2/HelloController.java
+++ composeTest2/src/main/java/com/example/composetest2/HelloController.java
@@ -2,10 +2,12 @@ package com.example.composetest2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://host.docker.internal:8081")
 public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<MsgDto> hello(){
